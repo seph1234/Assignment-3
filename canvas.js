5d5b9109ef2cd8008776acd5d4866aeaf312bc02
pwrtwo <pwr_two@hotmail.com>
2016-11-10 23:39:48 -0800

    added bricks and some minor fix

--- canvas.js
+++ canvas.js
@@ -6,7 +6,8 @@ drawHouse();
 //drawSmoke2();
 //drawSmoke3();
 //drawSmoke4();
-// fuction for drawing ground
+
+// function for drawing ground
 function drawGround() {
 var centerX = 0;
 var centerY = 0;
@@ -17,7 +18,7 @@ ctx.save();
 // translate context so height is 1/3'rd from top of enclosing circle
 ctx.translate(canvas.width / 2, canvas.height / 2 + 100);
 // scale context horizontally by 300%
-ctx.scale(3, 1);
+ctx.scale(6, 1);
 // draw circle which will be stretched into an oval
 ctx.beginPath();
 ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
@@ -40,11 +41,11 @@ var x = canvas.width / 2;
 var y = canvas.height / 2;
 var grd = ctx.createRadialGradient(250, 250, 50, 250, 250, 180);
 
-// sidewall
+// side wall
 ctx.beginPath();
 ctx.moveTo(x,y +110);
-ctx.lineTo(x - 100,y + 70);
-ctx.lineTo(x - 100,y - 20);
+ctx.lineTo(x - 100,y + 60);
+ctx.lineTo(x - 100,y - 30);
 ctx.lineTo(x,y + 20);
 ctx.closePath();
 grd.addColorStop(0, 'brown');
@@ -52,6 +53,170 @@ grd.addColorStop(1, 'black');
 ctx.fillStyle = grd;
 ctx.fill();
 ctx.lineWidth = 1;
+ctx.strokeStyle = 'black';
+ctx.stroke();
+
+// side wall bricks
+ctx.beginPath();
+ctx.moveTo(x,y + 100);
+ctx.lineTo(x - 100,y + 50);
+
+ctx.moveTo(x,y + 90);
+ctx.lineTo(x - 100,y + 40);
+
+ctx.moveTo(x,y + 80);
+ctx.lineTo(x - 100,y + 30);
+
+ctx.moveTo(x,y + 70);
+ctx.lineTo(x - 100,y + 20);
+
+ctx.moveTo(x,y + 60);
+ctx.lineTo(x - 100,y + 10);
+
+ctx.moveTo(x,y + 50);
+ctx.lineTo(x - 100,y);
+
+ctx.moveTo(x,y + 40);
+ctx.lineTo(x - 100,y - 10);
+
+ctx.moveTo(x,y + 30);
+ctx.lineTo(x - 100,y - 20);
+
+// pattern 1
+ctx.moveTo(x-10,y + 10);
+ctx.lineTo(x-10,y + 25);
+
+ctx.moveTo(x-30,y);
+ctx.lineTo(x-30,y + 15);
+
+ctx.moveTo(x-50,y - 10);
+ctx.lineTo(x-50,y + 5);
+
+ctx.moveTo(x-70,y - 20);
+ctx.lineTo(x-70,y - 5);
+
+ctx.moveTo(x-90,y - 30);
+ctx.lineTo(x-90,y - 15);
+
+///////////////////////2
+ctx.moveTo(x-10,y + 45);
+ctx.lineTo(x-10,y + 35);
+
+ctx.moveTo(x-30,y + 25);
+ctx.lineTo(x-30,y + 35);
+
+ctx.moveTo(x-50,y + 15);
+ctx.lineTo(x-50,y + 25);
+
+ctx.moveTo(x-70,y + 15);
+ctx.lineTo(x-70,y + 5);
+
+ctx.moveTo(x-90,y - 5);
+ctx.lineTo(x-90,y + 5);
+
+///////////////////////3
+ctx.moveTo(x-10,y + 65);
+ctx.lineTo(x-10,y + 55);
+
+ctx.moveTo(x-30,y + 45);
+ctx.lineTo(x-30,y + 55);
+
+ctx.moveTo(x-50,y + 35);
+ctx.lineTo(x-50,y + 45);
+
+ctx.moveTo(x-70,y + 35);
+ctx.lineTo(x-70,y + 25);
+
+ctx.moveTo(x-90,y + 15);
+ctx.lineTo(x-90,y + 25);
+
+///////////////////////4
+ctx.moveTo(x-10,y + 85);
+ctx.lineTo(x-10,y + 75);
+
+ctx.moveTo(x-30,y + 65);
+ctx.lineTo(x-30,y + 75);
+
+ctx.moveTo(x-50,y + 55);
+ctx.lineTo(x-50,y + 65);
+
+ctx.moveTo(x-70,y + 55);
+ctx.lineTo(x-70,y + 45);
+
+ctx.moveTo(x-90,y + 35);
+ctx.lineTo(x-90,y + 45);
+
+///////////////////////5
+ctx.moveTo(x-10,y + 105);
+ctx.lineTo(x-10,y + 95);
+
+ctx.moveTo(x-30,y + 85);
+ctx.lineTo(x-30,y + 95);
+
+ctx.moveTo(x-50,y + 75);
+ctx.lineTo(x-50,y + 85);
+
+ctx.moveTo(x-70,y + 75);
+ctx.lineTo(x-70,y + 65);
+
+ctx.moveTo(x-90,y + 55);
+ctx.lineTo(x-90,y + 65);
+
+
+// pattern 2
+ctx.moveTo(x-80,y - 10);
+ctx.lineTo(x-80,y + 0);
+
+ctx.moveTo(x-60,y + 10);
+ctx.lineTo(x-60,y + 0);
+
+ctx.moveTo(x-40,y + 20);
+ctx.lineTo(x-40,y + 10);
+
+ctx.moveTo(x-20,y + 30);
+ctx.lineTo(x-20,y + 20);
+
+///////////////////////2
+ctx.moveTo(x-80,y + 20);
+ctx.lineTo(x-80,y + 10);
+
+ctx.moveTo(x-60,y + 30);
+ctx.lineTo(x-60,y + 20);
+
+ctx.moveTo(x-40,y + 40);
+ctx.lineTo(x-40,y + 30);
+
+ctx.moveTo(x-20,y + 50);
+ctx.lineTo(x-20,y + 40);
+
+///////////////////////3
+ctx.moveTo(x-80,y + 40);
+ctx.lineTo(x-80,y + 30);
+
+ctx.moveTo(x-60,y + 50);
+ctx.lineTo(x-60,y + 40);
+
+ctx.moveTo(x-40,y + 60);
+ctx.lineTo(x-40,y + 50);
+
+ctx.moveTo(x-20,y + 70);
+ctx.lineTo(x-20,y + 60);
+
+///////////////////////4
+ctx.moveTo(x-80,y + 60);
+ctx.lineTo(x-80,y + 50);
+
+ctx.moveTo(x-60,y + 70);
+ctx.lineTo(x-60,y + 60);
+
+ctx.moveTo(x-40,y + 80);
+ctx.lineTo(x-40,y + 70);
+
+ctx.moveTo(x-20,y + 90);
+ctx.lineTo(x-20,y + 80);
+
+ctx.closePath();
+ctx.lineWidth = 0.3;
 ctx.strokeStyle = 'black';
 ctx.stroke();
 
@@ -67,18 +232,90 @@ ctx.lineWidth = 1;
 ctx.strokeStyle = 'black';
 ctx.stroke();
 
+// body bricks
+ctx.beginPath();
+ctx.moveTo(x, y + 30);
+ctx.lineTo(x + 90, y + 30);
+ctx.moveTo(x, y + 40);
+ctx.lineTo(x + 90, y + 40);
+ctx.moveTo(x, y + 50);
+ctx.lineTo(x + 90, y + 50);
+ctx.moveTo(x, y + 60);
+ctx.lineTo(x + 90, y + 60);
+ctx.moveTo(x, y + 70);
+ctx.lineTo(x + 90, y + 70);
+ctx.moveTo(x, y + 80);
+ctx.lineTo(x + 90, y + 80);
+ctx.moveTo(x, y + 90);
+ctx.lineTo(x + 90, y + 90);
+ctx.moveTo(x, y + 100);
+ctx.lineTo(x + 90, y + 100);
+
+ctx.moveTo(x + 30, y + 10);
+ctx.lineTo(x + 30, y + 30);
+ctx.moveTo(x + 60, y + 10);
+ctx.lineTo(x + 60, y + 30);
+
+ctx.moveTo(x + 15, y + 30);
+ctx.lineTo(x + 15, y + 40);
+ctx.moveTo(x + 45, y + 30);
+ctx.lineTo(x + 45, y + 40);
+ctx.moveTo(x + 75, y + 30);
+ctx.lineTo(x + 75, y + 40);
+
+ctx.moveTo(x + 15, y + 50);
+ctx.lineTo(x + 15, y + 60);
+ctx.moveTo(x + 45, y + 50);
+ctx.lineTo(x + 45, y + 60);
+ctx.moveTo(x + 75, y + 50);
+ctx.lineTo(x + 75, y + 60);
+
+ctx.moveTo(x + 30, y + 60);
+ctx.lineTo(x + 30, y + 70);
+ctx.moveTo(x + 60, y + 60);
+ctx.lineTo(x + 60, y + 70);
+
+ctx.moveTo(x + 15, y + 70);
+ctx.lineTo(x + 15, y + 80);
+ctx.moveTo(x + 45, y + 70);
+ctx.lineTo(x + 45, y + 80);
+ctx.moveTo(x + 75, y + 70);
+ctx.lineTo(x + 75, y + 80);
+
+ctx.moveTo(x + 30, y + 80);
+ctx.lineTo(x + 30, y + 90);
+ctx.moveTo(x + 60, y + 80);
+ctx.lineTo(x + 60, y + 90);
+
+ctx.moveTo(x + 15, y + 90);
+ctx.lineTo(x + 15, y + 100);
+ctx.moveTo(x + 45, y + 90);
+ctx.lineTo(x + 45, y + 100);
+ctx.moveTo(x + 75, y + 90);
+ctx.lineTo(x + 75, y + 100);
+
+ctx.moveTo(x + 30, y + 100);
+ctx.lineTo(x + 30, y + 110);
+ctx.moveTo(x + 60, y + 100);
+ctx.lineTo(x + 60, y + 110);
+
+ctx.closePath();
+ctx.lineWidth = 0.3;
+ctx.strokeStyle = 'black';
+ctx.stroke();
+
 // window1
 ctx.beginPath();
 ctx.rect(x + 20, y + 40, 20, 20);
 ctx.closePath();
-ctx.fillStyle = 'white';
+ctx.fillStyle = "#A1E3FC";
 ctx.fill();
 
 // window2
 ctx.beginPath();
 ctx.rect(x + 60, y + 40, 20, 20);
 ctx.closePath();
-ctx.fillStyle = 'white';
+ctx.fillStyle = '#A1E3FC';
 ctx.fill();
 
 // window lines
@@ -100,10 +337,10 @@ ctx.stroke();
 ctx.beginPath();
 ctx.rect(x + 40, y + 80, 20, 30);
 ctx.closePath();
-ctx.fillStyle = 'white';
+ctx.fillStyle = '#944A00';
 ctx.fill();
 
-// doornob
+// doorknob
 ctx.beginPath();
 ctx.arc(x + 55, y + 100, 2, 0, 2*Math.PI);
 ctx.fillStyle = 'black';
@@ -115,9 +352,10 @@ ctx.moveTo(x,y + 20);
 ctx.lineTo(x + 90,y + 20);
 ctx.lineTo(x + 50,y - 10);
 ctx.lineTo(x, y + 20);
+
 ctx.moveTo(x + 50, y - 10);
-ctx.lineTo(x - 30, y - 40);
-ctx.lineTo(x - 100, y - 20);
+ctx.lineTo(x - 50, y - 50);
+ctx.lineTo(x - 100, y - 30);
 ctx.lineTo(x, y + 20);
 ctx.closePath();
 ctx.fillStyle = 'black';
@@ -220,9 +458,10 @@ ctx.strokeStyle = 'black';
 ctx.stroke();
 }
 
-var counter = setInterval (function() {
 
- document.querySelector('input[type=range]').value = 50;
+ var counter = setInterval (function() {
+
+ document.getElementById("mySlider").value = 50;
 
  counter++;
  if (counter % 3 == 1) {
@@ -238,7 +477,8 @@ var counter = setInterval (function() {
 	drawSmoke();
  }
 }, 1000);
-//asfdklsajfk
+
+
 
 
 
